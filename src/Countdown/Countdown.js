cc5d64276ea4e424b944d036023df0df587ae6f8
Szocs Bence Barnabas <bencebarnabasszocs@gmail.com>
2024-07-27 00:40:48 +0200

    countdown

--- src/Countdown/Countdown.js
+++ src/Countdown/Countdown.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import Marquee from "react-fast-marquee";
 
 function Countdown() {
-  const targetDate = new Date('2024-07-31T18:00:00');
+  const targetDate = new Date('2024-08-T28:00:00');
   const [email,setEmail] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
   const [isLoaded,setIsLoaded] = useState(false);
